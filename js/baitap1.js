ed622057a7afe11ea040291535ce3e79d2d2a4bc
viettoanle2304 <vietoan230499@gmail.com>
2022-03-05 17:01:06 +0700

    bai tap 1 - bo sung comment

--- js/baitap1.js
+++ js/baitap1.js
@@ -1,3 +1,12 @@
+/*
+ ** Đầu vào: Số ngày làm
+ ** Các bước xử lí:
+ ** B1: Tạo 1 biến có giá trị không đổi là lương một ngày = 100.000
+ ** B2: Nhận dữ liệu về số ngày làm từ người dùng
+ ** B3: In ra kết quả lương = lương một ngày * số ngày làm
+ ** Đầu ra: Lương tổng cộng
+ */
+
 const luong1Ngay = 100000;
 let soNgayLam = prompt("Nhập số ngày làm: ");
 console.log("Lương: ", luong1Ngay * soNgayLam);
